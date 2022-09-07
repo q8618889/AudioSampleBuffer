d855897809cea8bb4bca4dff41d811018cccf63b
孟凡杰 <675632487@qq.com>
2022-09-07 10:26:30 +0800

    Initial Commit

---
+++ AudioSampleBuffer/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  AudioSampleBuffer
+//
+//  Created by gt on 2022/9/7.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ AudioSampleBuffer/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  AudioSampleBuffer
+//
+//  Created by gt on 2022/9/7.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
